ee940c8b4fdbb7c94ca08321f914a33de679cdf8
redth <jondick@gmail.com>
2023-03-16 23:32:06 -0400

    Fix SvgImageSourceConverter
    Converters in MAUI changed slightly, this fixes things so that svg file patterns can be detected again in the converter.

--- source/FFImageLoading.Svg.Maui/SvgImageSourceConverter.cs
+++ source/FFImageLoading.Svg.Maui/SvgImageSourceConverter.cs
@@ -1,66 +1,42 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using FFImageLoading.Maui;
 
 
 namespace FFImageLoading.Svg.Maui
 {
-    [Preserve(AllMembers = true)]
-    /// <summary>
-    /// SvgImageSourceConverter
-    /// </summary>
-    public class SvgImageSourceConverter : FFImageLoading.Maui.ImageSourceConverter, IValueConverter
-    {
-        /// <summary>
-        /// Convert
-        /// </summary>
-        /// <param name="value"></param>
-        /// <param name="targetType"></param>
-        /// <param name="parameter"></param>
-        /// <param name="culture"></param>
-        /// <returns></returns>
-        public new object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            var str = value as string;
-            if (string.IsNullOrWhiteSpace(str))
-                return null;
-
-            return base.Convert(value, targetType, parameter, culture);
-        }
-
-        /// <summary>
-        /// ConvertBack
-        /// </summary>
-        /// <param name="value"></param>
-        /// <param name="targetType"></param>
-        /// <param name="parameter"></param>
-        /// <param name="culture"></param>
-        /// <returns></returns>
-        public new object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
-
+	[Preserve(AllMembers = true)]
+	/// <summary>
+	/// SvgImageSourceConverter
+	/// </summary>
+	public class SvgImageSourceConverter : FFImageLoading.Maui.ImageSourceConverter, IValueConverter
+	{
 		public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+			=> sourceType == typeof(string);
+
+		//public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+		//	=> CoreConvertFrom(value);
+
+
+		public new object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+			=> CoreConvertFrom(value);
+
+		object CoreConvertFrom(object value)
 		{
-			return sourceType == typeof(string);
+			var text = value as string;
+			if (string.IsNullOrWhiteSpace(text))
+				return null;
+
+			var xfSource = base.ConvertFrom(text) as ImageSource;
+
+			if (text.IsSvgFileUrl() || text.IsSvgDataUrl())
+			{
+				return new SvgImageSource(xfSource, 0, 0, true);
+			}
+
+			return xfSource;
 		}
-
-		public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
-		{
-            var text = value as string;
-            if (string.IsNullOrWhiteSpace(text))
-                return null;
-
-            var xfSource = base.ConvertFrom(text) as ImageSource;
-
-            if (text.IsSvgFileUrl() || text.IsSvgDataUrl())
-            {
-                return new SvgImageSource(xfSource, 0, 0, true);
-            }
-
-            return xfSource;
-        }
-    }
+	}
 }
